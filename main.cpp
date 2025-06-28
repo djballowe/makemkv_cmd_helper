b0171508a6891442c34bea6e4d125efbf24cf7c6
djballowe <djballowe@gmail.com>
2025-06-27 22:23:32 -0700

    new regex

--- main.cpp
+++ main.cpp
@@ -30,28 +30,31 @@ std::vector<std::string> split(std::string split_string, char delimiter) {
 }
 
 void parseTitle(std::map<int, std::pair<std::string, int>> &titles, std::string buffer) {
-    std::vector<std::string> split_output = split(buffer, '"');
-    if (split_output.size() != 3) {
-        return;
-    }
+    std::regex progress("/TINFO:([0-9]+),[0-9]+,[0-9]+,\"(.+ - [0-9]+ chapter\(s\\)) , ([0-9]*(?:[.][0-9]+)?) GB\"/gm");
+    std::smatch progress_match;
 
-    std::string title_line = split_output[0];
-    std::string name_size = split_output[split_output.size() - 2];
-
-    size_t has_chapters = name_size.find("chapter(s)");
-    size_t has_GB = name_size.find("GB");
-    if (has_chapters == std::string::npos || has_GB == std::string::npos) {
-        return;
-    }
-
-    std::vector<std::string> name_size_split = split(name_size, ',');
-    std::vector<std::string> title_line_split = split(title_line, ':');
-    std::vector<std::string> title = split(title_line_split[1], ',');
-    int title_number = stoi(title[0]);
-    float_t title_size_GB = stof(name_size_split[1]);
-    std::string name = name_size_split[0];
-
-    titles[title_number] = {name, title_size_GB};
+    // std::vector<std::string> split_output = split(buffer, '"');
+    // if (split_output.size() != 3) {
+    //     return;
+    // }
+    //
+    // std::string title_line = split_output[0];
+    // std::string name_size = split_output[split_output.size() - 2];
+    //
+    // size_t has_chapters = name_size.find("chapter(s)");
+    // size_t has_GB = name_size.find("GB");
+    // if (has_chapters == std::string::npos || has_GB == std::string::npos) {
+    //     return;
+    // }
+    //
+    // std::vector<std::string> name_size_split = split(name_size, ',');
+    // std::vector<std::string> title_line_split = split(title_line, ':');
+    // std::vector<std::string> title = split(title_line_split[1], ',');
+    // int title_number = stoi(title[0]);
+    // float_t title_size_GB = stof(name_size_split[1]);
+    // std::string name = name_size_split[0];
+    //
+    // titles[title_number] = {name, title_size_GB};
     return;
 }
 
@@ -68,8 +71,10 @@ std::string buildRipCommand(const char *command) {
     std::array<char, 256> buffer;
 
     while (fgets(buffer.data(), sizeof(buffer), fp) != NULL) {
-        if (buffer[0] == 'T') {
-            parseTitle(titles, buffer.data());
+        std::string line(buffer.data());
+        if (line[0] == 'T') {
+            std::cout << line << std::endl;
+            parseTitle(titles, line);
         }
     }
 
@@ -173,6 +178,3 @@ int main() {
 
     return 0;
 }
-
-// TINFO:0,30,0,"Inglourious Basterds - 1
-// chapter(s) , 64.3 MB
